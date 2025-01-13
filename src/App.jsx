eb95b30ab8cc28aa21d42db33be8110d943f50c9
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2025-01-13 16:38:16 -0600

    Start initial layout for landing page and gage page with mock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import bootstrap css
-import Accordion from "react-bootstrap/Accordion";
-import { LRGS_DOMAIN } from "./constants";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ReportSelect from "./components/ReportSelect";
+import Report from "./components/Report";
+
+const queryClient = new QueryClient();
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(`${LRGS_DOMAIN}/dcp`)
-      .then((res) => res.json())
-      .then(setUsers);
-  }, []);
+  const [form, setForm] = useState(null);
 
   return (
-    <div className="p-5">
-      <h2>DCPMon</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.timestamp}>{user.timestamp}</li>
-        ))}
-      </ul>
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="p-5">
+        <h2>DCPMon</h2>
+        {!form ? <ReportSelect onForm={setForm} /> : <Report />}
+      </div>
+    </QueryClientProvider>
   );
 }
 
